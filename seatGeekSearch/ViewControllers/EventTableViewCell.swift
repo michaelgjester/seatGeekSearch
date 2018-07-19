0e91a85623d1b89bbea1b5b3ca37bcc29c1d6113
Michael Jester <michaeljester@michaels-macbook-pro.local>
2018-07-19 10:25:38 -0500

    cleanup/move defunct viewController to trash

--- seatGeekSearch/ViewControllers/EventTableViewCell.swift
+++ seatGeekSearch/ViewControllers/EventTableViewCell.swift
@@ -15,12 +15,10 @@ class EventTableViewCell: UITableViewCell {
   override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        //eventTitleLabel.text = "default"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
